8ba7e6d916abc7d42203bedc23ac5f104d609179
Seth M Cohen <smcohen82@gmail.com>
2021-05-11 09:25:27 -0400

    Begin login functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
+import React, { useState }from 'react'
 import {Switch, Route} from 'react-router-dom'
 import PostList from './PostList'
+import Login from './Login'
 
-function App() {
+function App() { 
+  const [currentUserId, setCurrentUserId] = useState("")
+
+ 
   return (
     <Switch>
       <Route exact path="/">
-        <PostList />
+        {currentUserId ? <PostList /> : <Login setCurrentUserId={setCurrentUserId}/>}
+      </Route>
+      <Route path="/users">
+        <h1>something</h1>
       </Route>
     </Switch>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function Login({ setCurrentUserId }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+  const [errors, setErrors] = useState([])
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch('http://localhost:3000/users/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then((data) => {
+      if (data.id){
+        setCurrentUserId(data.id)
+      } else {
+        setErrors(data)
+      }
+    })
+  }
+
+  return (
+    <div>
+      {errors ? <p style={{color: "red"}}>{errors[0]}</p> : null}
+      <form onSubmit={handleSubmit}>
+        <label>Email: </label>
+        <input type="text" name="email" value={formData.email} onChange={handleChange}/><br />
+        <label>Password: </label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange}/><br />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Login 
