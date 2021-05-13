3ceb6cb6e0ae2d1f1c7df6a01b455f0b56d252ca
coltenappleby <colten.appleby@gmail.com>
2021-05-13 13:56:46 -0400

    almost done with postcreate

--- src/components/PostEdit.js
+++ src/components/PostEdit.js
@@ -6,7 +6,16 @@ import PostCard from './PostCard'
 function PostEdit(){
 
     const { id } = useParams();
-    const [formData, setFormData] = useState([])
+    const [postData, setPostData] = useState([])
+    const [formData, setFormData] = useState({
+        title: "",
+        media_link: "",
+        media_type: "image/gif",
+        team1: "",
+        team2: "",
+        description: "",
+        user_id: window.sessionStorage.getItem("currentUserId")
+    })
 
     const teams = ['Arizona Diamondbacks','Atlanta Braves','Baltimore Orioles','Boston Red Sox','Chicago Cubs','Chicago White Sox','Cincinnati Reds','Cleveland Indians','Colorado Rockies','Detroit Tigers','Houston Astros','Kansas City Royals','Los Angeles Angels','Los Angeles Dodgers','Miami Marlins','Milwaukee Brewers','Minnesota Twins','New York Mets','New York Yankees','Oakland Athletics','Philadelphia Phillies','Pittsburgh Pirates','San Diego Padres','San Francisco Giants','Seattle Mariners','St. Louis Cardinals','Tampa Bay Rays','Texas Rangers','Toronto Blue Jays','Washington Nationals']
     const teamSelectOptions = teams.map((team, index) => <option key={team + index} value={team}>{team}</option>)
@@ -15,49 +24,64 @@ function PostEdit(){
     useEffect(() => {
         fetch(`http://localhost:3000/posts/${id}`)
         .then(res => res.json())
-        .then(postData => setFormData(postData))
+        .then(data => {
+            setPostData([data])
+            setFormData(data)
+        })
     }, [])
 
-    console.log(formData)
+    console.log(postData)
 
     function handleChange(e) {
-
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
     }
 
     function handleSubmit(e){}
 
+    const postCards = postData.map((post) => {
+        return (
+            <div className="post-cards-container">
+                <PostCard key={post.id} {...post}/>
+            </div>
+        )
+      })
+
+
     return(
         <div>
-        <h1> {id} </h1> 
+            <h1> {id} </h1> 
 
-        <div> 
-          <PostCard {...formData} />
+            {postCards}
+
+            <form onSubmit={handleSubmit}>
+                <label>Title: </label><br/>
+                    <input type="text" name="title" value={formData.title} onChange={handleChange}/><br/>
+                <label>Post Type: </label><br/>
+                    <select name="media_type" onChange={handleChange}>
+                        <option value="image/gif">Image/Gif</option>
+                        <option value="text">Text</option>
+                        <option value="video">Video</option>
+                    </select><br/>
+                <label>Team1 (required): </label><br/>
+                    <select name="team1" onChange={handleChange}>
+                        {teamSelectOptions}
+                    </select><br/>
+                <label>Team2 (optional): </label><br/>
+                    <select name="team2" onChange={handleChange}>
+                        {teamSelectOptions}
+                    </select><br/>
+                <label>Media Link: </label><br/>
+                    <textarea name="media_link" value={formData.media_link} onChange={handleChange}/><br/>
+                <label>Description: </label><br/>
+                    <textarea name="description" value={formData.description} onChange={handleChange}/><br/>
+                <input type="submit" />
+            </form> 
+
+                                
         </div>
-
-        <form onSubmit={handleSubmit}>
-            <label>Title: </label><br/>
-                <input type="text" name="title" value={formData.title} onChange={handleChange}/><br/>
-            <label>Post Type: </label><br/>
-                <select name="media_type" onChange={handleChange}>
-                    <option value="image/gif">Image/Gif</option>
-                    <option value="text">Text</option>
-                    <option value="video">Video</option>
-                </select><br/>
-            <label>Team1 (required): </label><br/>
-            <select name="team1" onChange={handleChange}>
-                {teamSelectOptions}
-            </select><br/>
-            <label>Team2 (optional): </label><br/>
-            <select name="team2" onChange={handleChange}>
-                {teamSelectOptions}
-            </select><br/>
-            <label>Media Link: </label><br/>
-            <textarea name="media_link" value={formData.mediaLink} onChange={handleChange}/><br/>
-            <label>Description: </label><br/>
-            <textarea name="description" value={formData.description} onChange={handleChange}/><br/>
-            <input type="submit" />
-        </form>
-    </div>
     )
 
 
